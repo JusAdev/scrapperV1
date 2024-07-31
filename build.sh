86f907b0f38ec58e518f9b60174e425b648d51da
JusAdev <udartdesign@gmail.com>
2024-07-31 20:32:43 -0300

    v2  build.sh

--- build.sh
+++ build.sh
@@ -2,17 +2,21 @@
 # exit on error
 set -o errexit
 
-# Set the environment variable if not already set - ok
+# Set the environment variable if not already set
 export PUPPETEER_CACHE_DIR=${PUPPETEER_CACHE_DIR:-/opt/render/project/puppeteer}
 
 npm install
 # npm run build # Uncomment this line if you have a build step
 
+# Ensure the Puppeteer cache directory exists
+mkdir -p $PUPPETEER_CACHE_DIR
+mkdir -p $XDG_CACHE_HOME/puppeteer
+
 # Store/pull Puppeteer cache with build cache
-if [[ ! -d $PUPPETEER_CACHE_DIR ]]; then 
+if [[ ! "$(ls -A $PUPPETEER_CACHE_DIR)" ]]; then 
   echo "...Copying Puppeteer Cache from Build Cache" 
-  cp -R $XDG_CACHE_HOME/puppeteer/ $PUPPETEER_CACHE_DIR
+  cp -R $XDG_CACHE_HOME/puppeteer/. $PUPPETEER_CACHE_DIR/
 else 
   echo "...Storing Puppeteer Cache in Build Cache" 
-  cp -R $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
+  cp -R $PUPPETEER_CACHE_DIR/. $XDG_CACHE_HOME/puppeteer/
 fi
